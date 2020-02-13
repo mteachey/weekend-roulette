32d534260aa63705ce4b75d75240234910f3540a
mteachey <melindateachey@gmail.com>
2020-02-13 14:29:17 -0700

    pseudocode of js for input screen

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 'use strict';
+//Pseudocode for Input Screen
+//load watchFormSubmit function onload
+//listen for submit event (click button or enter), prevent default
+//retrieve input values and store in an array
+//call the function to format the parameters and header of the api calls
+//pass these parameters and header to the api call functions (one function/api) 
+    //these functions will combine the parameters with the api url and then call the api using fetch; throwing an error for no reponse and catching errors with error message
+//then call the disaplyResults function
+    //this function will call the pickResult function which will select 2 random options out of the results returned by the api(s)
+    //then render/display these results plus a start over button, home button, learn more button
+
+//
+
+$(watchFormSubmit);
