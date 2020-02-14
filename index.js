0d377dd6c99929013c2b6aed12f84f410d9fc9af
mteachey <melindateachey@gmail.com>
2020-02-14 11:16:17 -0700

    first api function written to get 2 random hikes

--- index.js
+++ index.js
@@ -12,22 +12,42 @@
 //
 
 //Going to start with a const latLong then add the user input and api call 
-const lat = 40.0274;
-const lon = -105.2519;
+const latitude = 40.0274;
+const longitude = -105.2519;
 //remove once set up getlatLong
 
 //input vars//
-//radius-day, min-length,hiking(yes), mountain-biking(yes),
+//radius-day, min-length,hiking(yes), mountain-biking(yes),radius-night
 
 const hikeKey = '200684713-dde33619c13cd28faa7456223edcf195';
 const bikeKey = '200684713-dde33619c13cd28faa7456223edcf195';
 const restaurantHeader = 'user-key:e5800b1de7b26545fe07ad6a49160396';
 const latLongKey = '6c790e07e8a9409b98ba70f8cbde2eab';
 
-const hikeUrl = 'https://www.hikingproject.com/data/get-trails';
-const bikeUrl = 'https://www.mtbproject.com/data/get-trails';
-const restaurantUrl = 'https://developers.zomato.com/api/v2.1/search';
-const latLongUrl = 'https://api.opencagedata.com/geocode/v1/json';
+const hikeEndPoint = 'https://www.hikingproject.com/data/get-trails';
+const bikeEndPoint = 'https://www.mtbproject.com/data/get-trails';
+const restaurantEndPoint = 'https://developers.zomato.com/api/v2.1/search';
+const latLongEndPoint = 'https://api.opencagedata.com/geocode/v1/json';
+
+let dayActivity = [];
+
+
+function resultsHiking(responseJsonHiking){
+    const numberOfResults = responseJsonHiking.trails.length;
+    const firstTrailNumber = Math.floor(Math.random() * (numberOfResults+1));     
+    const secondTrailNumber = Math.floor(Math.random() * (numberOfResults+1));
+    
+    dayActivity.push(responseJsonHiking.trails[firstTrailNumber]);
+    dayActivity.push(responseJsonHiking.trails[secondTrailNumber]);
+    
+    console.log(firstTrailNumber);
+    console.log(`numberOfResults is ${numberOfResults}`);
+    
+    console.log(`this is the first trail ${responseJsonHiking.trails[firstTrailNumber].name}`)
+    console.log(`this is the dayActivity array ${dayActivity[0].name} and ${dayActivity[1].name}`);
+    console.log(`resultsHiking ran`);
+}
+
 
 function pickResultsDay(){
  //this function will take in the results from the day apis and then select 2 random options out of those results to returned   
@@ -45,11 +65,12 @@ function displayResults(){
     console.log(`displayResultsRan`);
 }
 
-
-function formatParamaters(){
+function formatParameters(params){
 //take in input from forms; lat/long and format parameters and headers (if necessary) for each API
 //pass these parameters (and headers) to the api call function (one function will then call all API functions)
-    console.log(`formatParamaters ran`);
+console.log(`formatParamaters ran`);
+const queryitems = Object.keys(params).map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+return queryitems.join('&');    
 }
 
 //API functions
@@ -59,6 +80,7 @@ function getLatLong(){
 }
 
 function getRestaurants(){
+   
     console.log(`getRestaurants ran`);
 }
 
@@ -66,13 +88,47 @@ function getBikes(){
     console.log(`getBikes ran`);
 }
 
-function getHikes(){
+function getHikes(radiusDay=20,length=0){
+    const params= {
+        lat: latitude,
+        lon: longitude,
+        maxDistance:radiusDay,
+        minLength: length,
+        maxResults:'100',
+        key: hikeKey,   
+        };
+    const url = `${hikeEndPoint}?lat=${latitude}&lon=${longitude}&maxDistance=${radiusDay}&minLength=${length}&maxResults=100&key=${hikeKey}`;
+    console.log(`hike url ${url}`);
+
+   fetch(url)
+     .then(response=>{
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJsonHiking=>{
+         console.log(`hike called worked`);
+         resultsHiking(responseJsonHiking);
+      })
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong. Try again in a bit`)
+     });
+     
     console.log(`getHikes ran`);
 }
 
-function callAPIs(){
+
+function callAPIs(radiusDay,length, hiking,mtnbiking,radiusNight){
  //call all of the individual API functions
- //then call the displayResults function   
+ //getLatLong();
+ //getRestaurants();
+ //getBikes();
+ //call getHikes only if hiking is checked
+ if(hiking==='yes'){
+ getHikes(radiusDay,length);}
+
+//then call the displayResults function   
     console.log(`callAPIs ran`);
 }
 //end of API functions
@@ -81,8 +137,18 @@ function callAPIs(){
 
 function watchFormSubmit(){
 //listen for submit event (click button or enter), prevent default
-//retrieve input values and store in an array
+//retrieve input values (and store in an object?)
 //call the function to format the parameters and header of the api calls
+$('#event-form').submit(function(event){
+    event.preventDefault();
+    let radiusDay = $('#radius-day').val();
+    let length = $('#min-length').val();
+    let hiking = $('#hiking').is(':checked')?'yes':'no';
+    let mtnbiking = $('#mountain-biking').is(':checked')?'yes':'no';
+    let radiusNight = $('#radius-night').val();
+    console.log(`rd${radiusDay} lenght${length} hiking${hiking} biking${mtnbiking} rn${radiusNight} `);
+    callAPIs(radiusDay, length, hiking,mtnbiking,radiusNight);
+});
     console.log(`watchFormSubmit ran`);
 }
 
